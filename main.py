cec23ab9b79838bf780c6d21401ecfc6442017a5
sunoopy <jjosephk99@gmail.com>
2024-11-30 12:44:18 +0900

    main file modified 1130

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-# main.py
-import numpy as np
-from HierFed.dataset import create_model, load_and_preprocess_data
-from fed import hierarchical_federated_learning, create_client_data_dirichlet
-from visualization_analysis import analyze_client_data, visualize_class_probabilities
+from federated_learning import HierFedLearning
 
-dataset = 'mnist'
-grid_size = 10
-clients_per_region = [10, 100, 2]
-alpha = 0.5
-samples_per_client = 100
-rounds = 2
-
-client_data, class_probs = create_client_data_dirichlet(dataset, grid_size, clients_per_region, alpha, samples_per_client)
-analyze_client_data(client_data, dataset, clients_per_region)
-
-random_x, random_y = np.random.randint(0, grid_size, 2)
-visualize_class_probabilities(class_probs, grid_size, random_x, random_y)
-
-final_model = hierarchical_federated_learning(dataset, grid_size, clients_per_region, alpha, samples_per_client, rounds)
+if __name__ == "__main__":
+    hierfed = HierFedLearning(
+        dataset_name="mnist",
+        total_rounds=100,
+        num_clients=100,
+        sample_per_client=100,
+        num_edge_servers=4,
+        grid_size=10,
+        alpha=100,
+        coverage_radius=3.0,
+        client_repetition=True
+    )
+    
+    hierfed.calculate_noniid_metrics()
+    hierfed.visualize_topology(show_grid=True, show_distances=True)
+    hierfed.visualize_edge_coverage()
+    final_model, history = hierfed.train()
+    hierfed.plot_training_metrics(history)
