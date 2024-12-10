80d13c0f4e60d0266d66c9b4510fdb608690ba3e
sunoopy <jjosephk99@gmail.com>
2024-12-10 16:19:27 +0900

    edge server location function file

--- utils.py
+++ utils.py
@@ -1,16 +1,27 @@
 import numpy as np
+from scipy.stats import dirichlet
+from collections import defaultdict
+
+def generate_edge_server_locations(grid_size, num_edge_servers):
+    edge_points = []
+    rows = int(np.sqrt(num_edge_servers))
+    cols = num_edge_servers // rows
+    for i in range(rows):
+        for j in range(cols):
+            x = (i + 0.5) * (grid_size / rows)
+            y = (j + 0.5) * (grid_size / cols)
+            edge_points.append((x, y))
+    return edge_points
+
+def generate_client_locations(grid_size, num_clients):
+    return [(np.random.uniform(0, grid_size), np.random.uniform(0, grid_size)) for _ in range(num_clients)]
+
+def generate_label_distributions(grid_size, num_classes, alpha):
+    distributions = {}
+    for i in range(grid_size):
+        for j in range(grid_size):
+            distributions[(i, j)] = dirichlet.rvs([alpha] * num_classes)[0]
+    return distributions
 
 def calculate_kl_divergence(p, q):
-    kl_div = 0
-    for i in range(len(p)):
-        if p[i] > 0 and q[i] > 0:
-            kl_div += p[i] * np.log(p[i] / q[i])
-    return kl_div
-
-def calculate_average_divergence(distributions):
-    avg_divergence = 0
-    num_distributions = len(distributions)
-    for i in range(num_distributions):
-        for j in range(i + 1, num_distributions):
-            avg_divergence += calculate_kl_divergence(distributions[i], distributions[j])
-    return avg_divergence / num_distributions
+    return sum(p[i] * np.log(p[i] / q[i]) for i in range(len(p)) if p[i] > 0 and q[i] > 0)
